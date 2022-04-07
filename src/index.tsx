303e2f39aed9b09edef2cef0e1b5a23aefa09dc9
torish14 <nanamin.104@icloud.com>
2022-04-08 06:56:56 +0900

    added a description that `document.getElementById('root')` corresponds to public/index.html

--- src/index.tsx
+++ src/index.tsx
@@ -8,8 +8,9 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
-);
+  // public/index.html の <div id="root"></div> に対応
+  document.getElementById('root'),
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
